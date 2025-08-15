d700f3c9cb92af518befc3239ca1faa8bd4482ce
Ankit Bhagat <bhagatankit502@gmail.com>
2025-08-15 20:59:15 +0530

    First commit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,16 +2,13 @@
 // import Footer from './Footer.jsx'
 // import Food from './Food.jsx'
 
-import Card from './Card.jsx'
+import Button from './Button.jsx'
 
 function App() {
 
     return (
     <>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Button />
     </>
     );
 
