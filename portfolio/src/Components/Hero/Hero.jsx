559e7f62b347e960e6a0abdf2dfc6dbdda503777
Ankit Bhagat <bhagatankit502@gmail.com>
2025-08-20 22:43:27 +0530

    Some Updated~

--- portfolio/src/Components/Hero/Hero.jsx
+++ portfolio/src/Components/Hero/Hero.jsx
@@ -6,7 +6,13 @@ const Hero = () => {
   return (
     <div className='hero'>
         <img src={profile_img} alt="" />
+        <h1><span>I'am Ankit Bhagat,</span> Data Analyst.</h1>
+        <p>Currently Pursuing B.Tech from SBJITMR.</p>
 
+        <div className="hero-action">
+            <div className="hero-connect">Connect with me</div>
+            <div className="hero-resume">My Resume</div>
+        </div>
 
     </div>
   )
