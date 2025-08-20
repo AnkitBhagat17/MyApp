57c1300bb911664625f54e3b2eeeecf7dc090f17
Ankit Bhagat <bhagatankit502@gmail.com>
2025-08-20 22:59:05 +0530

    Some Updated~

---
+++ portfolio/src/Components/About/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './About.css'
+
+export const About = () => {
+  return (
+    <div className='about'>
+        <div className="about-title">
+            <h1>About me</h1>
+            <img src="" alt="" />
+        </div>
+
+    </div>
+  )
+}
