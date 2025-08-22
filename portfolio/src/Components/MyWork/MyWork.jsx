3807f7b4e43a377c1ae6ec55a1fd44508c57c162
Ankit Bhagat <bhagatankit502@gmail.com>
2025-08-22 20:07:29 +0530

    Some Updated~

---
+++ portfolio/src/Components/MyWork/MyWork.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './MyWork.css';
+
+const MyWork = () => {
+  return (
+    <div className='mywork'>
+        <div className="mywork-title">
+            <h1>My Work</h1>
+        </div>
+        <div>
+            <div className="mywork-container">
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default MyWork
