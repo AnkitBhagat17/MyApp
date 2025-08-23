52b21656b2e27a8bd33e0654c128f9c182ce845b
Ankit Bhagat <bhagatankit502@gmail.com>
2025-08-23 17:41:48 +0530

    Some Updated~

--- portfolio/src/Components/MyWork/MyWork.jsx
+++ portfolio/src/Components/MyWork/MyWork.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import './MyWork.css';
+import mywork_data from '../../assets/my_work';
 
 const MyWork = () => {
   return (
-    <div className='mywork'>
-        <div className="mywork-title">
-            <h1>My Work</h1>
-        </div>
-        <div>
-            <div className="mywork-container">
-                
+    <div id="work" className="mywork">
+            <div className="mywork-title">
+                <h1>My Work</h1>
             </div>
+            <div className="work-container">
+                {mywork_data.map((work, index) => {
+                    return <img key={index} src={work.w_img} alt={work.w_name} className="work-image" />;
+                })}
+
+            </div>
+
         </div>
-    </div>
   )
 }
 
---
+++ portfolio/src/assets/my_work.js
@@ -0,0 +1,41 @@
+import project1_img from '../assets/Project1.png'
+import project2_img from '../assets/Project2.png'
+import project3_img from '../assets/Project3.png'
+import project4_img from '../assets/Project4.png'
+import project5_img from '../assets/Project5.png'
+import project6_img from '../assets/Project6.jpg'
+
+const mywork_data = [
+    {
+        w_no:1,
+        w_name:"Blinkit Sales Analysis Dashboard",
+        w_img:project1_img
+    },
+    {
+        w_no:2,
+        w_name:"Electric Vehicles Analysis Dashboard",
+        w_img:project2_img
+    },
+    {
+        w_no:3,
+        w_name:"Covid-19 Analysis",
+        w_img:project3_img
+    },
+    {
+        w_no:4,
+        w_name:"IPL Analysis 2008-2025",
+        w_img:project4_img
+    },
+    {
+        w_no:5,
+        w_name:"TCS Stock Analysis",
+        w_img:project5_img
+    },
+    {
+        w_no:6,
+        w_name:"Car Accident Blackbox",
+        w_img:project6_img
+    },
+]
+ 
+export default mywork_data;
