8d7e2b559f1b7bab01eef91865a56598a131965f
Ankit Bhagat <bhagatankit502@gmail.com>
2025-08-24 15:41:43 +0530

    Some Updated~

--- portfolio/src/Components/Navbar/Navbar.jsx
+++ portfolio/src/Components/Navbar/Navbar.jsx
@@ -1,28 +1,46 @@
-import React, { useState } from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo1.png'
-import underline from '../../assets/underline.png'
-import AnchorLink from "react-anchor-link-smooth-scroll";
+import React, { use } from "react";
+import { useState } from "react";
+import logo from "../../assets/logo1.png";
+import "./Navbar.css";
+import underline from "../../assets/underline.png";
+
 
 const Navbar = () => {
-  const [menu,setMenu] = useState("home");
+  const [menu, setMenu] = useState("about");
 
-  
+  const menuRef = useRef(null);
+
+  const openMenu = () => {
+    if (menuRef.current) {
+      menuRef.current.classList.add("active");
+
+    }
+   
+  };
+  const closeMenu = () => {
+    if (menuRef.current) {
+      menuRef.current.classList.remove("active");
+    }
+  };
+
   return (
-    <div className='navbar'>
-        <img src={logo} alt="" />
-        <ul className="nav-menu">
-            <li><AnchorLink className='anchor-link' href='#home'><p onClick={()=>setMenu("home")}>Home</p></AnchorLink>{menu==="home" ? <img src={underline} alt="" />:<></>}</li>
-            <li><AnchorLink className='anchor-link' href='#about'><p onClick={()=>setMenu("about")}>About Me</p></AnchorLink>{menu==="about" ? <img src={underline} alt="" />:<></>}</li>
-            <li><AnchorLink className='anchor-link' href='#services'><p onClick={()=>setMenu("services")}>Services</p></AnchorLink>{menu==="services" ? <img src={underline} alt="" />:<></>}</li>
-            <li><AnchorLink className='anchor-link' href='#work'><p onClick={()=>setMenu("work")}>Portfolio</p></AnchorLink>{menu==="work" ? <img src={underline} alt="" />:<></>}</li>
-            <li><AnchorLink className='anchor-link' href='#contact'><p onClick={()=>setMenu("contact")}>Contact</p></AnchorLink>{menu==="contact" ? <img src={underline} alt="" />:<></>}</li>
-        </ul>
-
-        <div className="nav-connect">Connect With Me</div>
-
-    </div>
-  )
+      <div className="navbar">
+      <img src={logo} alt="Logo" className="logo1" />
+      <img src={side_menu} alt="" onClick={openMenu} className="nav-open" />
+      <ul ref={menuRef} className="nav-menu">
+        <img src={close_x} alt="" onClick={closeMenu} className="nav-close" />
+            <li><AnchorLink className="anchor-link"  href="#home"><p onClick={()=> setMenu("home")}>Home</p></AnchorLink>{menu === "home"?<img src={underline} alt="" />:<> </>}</li>
+            <li><AnchorLink className="anchor-link"  href="#about"><p onClick={()=> setMenu("about")}>About Me</p></AnchorLink>{menu === "about"?<img src={underline} alt="" />:<></>}</li>
+            <li><AnchorLink className="anchor-link"  href="#services"><p onClick={()=> setMenu("services")}>Services</p></AnchorLink>{menu === "services"?<img src={underline} alt="" />:<></>}</li>
+            <li><AnchorLink className="anchor-link"  href="#work"><p onClick={()=> setMenu("work")}>Portfolio</p></AnchorLink>{menu === "work"?<img src={underline} alt="" />:<></>}</li>
+            <li><AnchorLink className="anchor-link"  href="#contact"><p onClick={()=> setMenu("contact")}>Contact</p></AnchorLink>{menu === "contact"?<img src={underline} alt="" />:<></>}</li>
+      </ul>
+      <div className="nav-connect"><AnchorLink className="anchor-link"  href="#contact">Connect With Me</AnchorLink></div>
+      
+      
+      
+      </div>
+      
+  );
 }
-
-export default Navbar
+export default Navbar;
